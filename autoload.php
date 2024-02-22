f9d15c0b5fe3ae4c98e52aa723b07c77fc2e87e6
Kiba <akasa2444@gmail.com>
2024-02-22 19:49:23 +0700

    install : composer mpdf

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/preps/Components/Helpers/helpers.php';
 require_once __DIR__ . '/preps/Components/Http/Request.php';
 require_once __DIR__ . '/preps/Components/Http/Response.php';
